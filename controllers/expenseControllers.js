bee09cb4edc5118e57579fab0b3c654cbe55395a
Prashant24081999 <prashantshukla1999@gmail.com>
2025-02-15 16:27:52 +0530

    updated delete expense function

--- controllers/expenseControllers.js
+++ controllers/expenseControllers.js
@@ -1,41 +1,51 @@
 const path = require("path");
 
+const sequelize = require("../util/database");
+
 const User = require("../models/userModel");
 const Expense = require("../models/expenseModel");
 
 exports.postExpense = async (req, res) => {
+  const t = await sequelize.transaction();
   const { amount, description, category } = req.body;
   const userId = req.userId;
 
   try {
-    const expense = await Expense.create({
-      amount: amount,
-      description: description,
-      category: category,
-      UserId: userId,
-    });
+    const expense = await Expense.create(
+      {
+        amount: amount,
+        description: description,
+        category: category,
+        UserId: userId,
+      },
+      { transaction: t }
+    );
 
     const user = await User.findByPk(userId);
     if (!user) {
+      await t.rollback();
       return res.status(404).json({ message: "User not found" });
     }
-    const totalExpense = user.totalExpense + amount;
+    const totalExpense = Number(user.totalExpense) + Number(amount);
     await User.update(
       {
         totalExpense: totalExpense,
       },
       {
         where: { id: userId },
+        transaction: t,
       }
     );
-
+    await t.commit();
     res.status(201).json(expense);
   } catch (err) {
+    await t.rollback();
     res.status(500).json({ message: "Error saving expense: ", err });
   }
 };
 
 exports.deleteExpense = async (req, res) => {
+  const t = await sequelize.transaction();
   const expenseId = req.params.expenseId;
   const userId = req.userId;
 
@@ -44,9 +54,22 @@ exports.deleteExpense = async (req, res) => {
       where: { expenseId: expenseId, UserId: userId },
     });
     if (expense) {
+      const user = await User.findByPk(userId);
+      const totalExpense = Number(user.totalExpense) - Number(expense.amount);
+      await User.update(
+        {
+          totalExpense: totalExpense,
+        },
+        {
+          where: { id: userId },
+          transaction: t,
+        }
+      );
+      await t.commit();
       await expense.destroy();
       res.status(200).json({ message: "Expense deleted successfully" });
     } else {
+      await t.rollback();
       res.status(404).json({ message: "Expense not found" });
     }
   } catch (err) {
